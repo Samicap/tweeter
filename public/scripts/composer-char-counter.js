f160621d15f949b320ba0a01fe2818651a01def5
Samicap <lovestochasewindmills@gmail.com>
2020-12-17 03:54:52 +0000

    refactor moved onKeyUp outside doc.ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
-  
-    $("#tweet-text").on('keyup', function(){
-      const charRemaining = 140 - $(this).val().length; //string
+$(document).ready(function () {
 
-      const tweetChar = $(".counter").val(charRemaining).val();
-      
-      if (tweetChar < 0) {
-        $(".counter").css('color', "red");
-        console.log("hi") 
-      } else {
-       $(".counter").css('color', "grey");
-      
-    }
-})
+  $("#tweet-text").on('keyup', onKeyUp);
 });
 
+const onKeyUp = function () {
+  const charRemaining = 140 - $(this).val().length; //string
+
+  const tweetChar = $(".counter").val(charRemaining).val();
+
+  if (tweetChar < 0) {
+    $(".counter").css('color', "red");
+    console.log("hi");
+  } else {
+    $(".counter").css('color', "grey");
+
+  }
+};
